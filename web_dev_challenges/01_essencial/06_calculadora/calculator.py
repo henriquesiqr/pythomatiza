177e6ed008e92ba9fa1edb8137a76626408fdcac
henriquesiqr <henrique.ribeiro4488@gmail.com>
2024-09-23 13:56:50 -0300

    add exercicios

--- web_dev_challenges/01_essencial/06_calculadora/calculator.py
+++ web_dev_challenges/01_essencial/06_calculadora/calculator.py
@@ -1,3 +1,7 @@
 def calculator():
     # TODO: Retorne o resultado da adição dos números informados pelo usuário
-    pass
+    number1 = input('Insira aqui o primeiro número da soma: ')
+    number2 = input('Insira aqui o segundo número da soma: ')
+    return number1 + number2
+
+
